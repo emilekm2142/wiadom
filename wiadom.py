2d97886dc41f18a5298d44954f3c327a9044ab19
emile <emilm7843@gmail.com>
2025-07-24 23:30:51 +0200

    2

--- wiadom.py
+++ wiadom.py
@@ -8,8 +8,10 @@ import sys, subprocess
 import os
 from datetime import datetime
 from pathlib import Path
-from playsound import playsound
+# Using Windows PowerShell for sound playback instead of playsound
 import threading
+import random
+import json
 
 # Set up the working directory and file paths
 p = Path(os.path.abspath(__file__)).parent
@@ -18,24 +20,76 @@ def play_sound(filename):
     sound_path = os.path.join(p, f"sounds/{filename}")
     subprocess.Popen(f'powershell -c (New-Object Media.SoundPlayer "{sound_path}").PlaySync();', shell=True)
 class Wiadom(object):
+    def __init__(self):
+        self.latest_response = None
+    
     @cherrypy.expose
     def index(self):
-        with open(os.path.join(p, "index.html")) as i:
+        with open(os.path.join(p, "index.html"), encoding='utf-8') as i:
             return i.read()
     
     @cherrypy.expose
-    def wiadom(self, msg=''):
+    def wiadom(self, msg='', picture='random', sound='random'):
         wiadomWindowFilePath = os.path.join(p, 'wiadomWindow.py')
         speakFilePath = os.path.join(p, 'speak.py')
         picturesPath = p
-        subprocess.Popen(f'python{w} "{wiadomWindowFilePath}" "{msg}" "{picturesPath}"')
-        play_sound(random.choice['wiadomosc_od_twojego_doradcy.wav', 'wiadomosc_od_szczura.wav'])
+        
+        # Handle picture selection
+        if picture == 'random':
+            selected_picture = 'random'
+        else:
+            selected_picture = picture
+            
+        # Handle sound selection
+        if sound == 'random':
+            selected_sound = random.choice(['wiadomosc_od_twojego_doradcy.wav', 'wiadomosc_od_szczura.wav'])
+        else:
+            selected_sound = sound
+            
+        subprocess.Popen(f'python{w} "{wiadomWindowFilePath}" "{msg}" "{picturesPath}" "{selected_picture}"')
+        play_sound(selected_sound)
       
         sleep(1.2)
         if 'obiad' in msg: 
             play_sound("moze_bys_cos_przekasil_sir.wav")
         subprocess.Popen(f'python{w} "{speakFilePath}" "{msg}"')
         return "ok"
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def response(self):
+        if cherrypy.request.method == 'POST':
+            input_json = cherrypy.request.json
+            self.latest_response = input_json.get('response')
+            return "ok"
+        return "method not allowed"
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_response(self):
+        response = self.latest_response
+        self.latest_response = None  # Clear after reading
+        return {"response": response}
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_pictures(self):
+        import glob
+        pictures = []
+        for ext in ['*.jpg', '*.jpeg', '*.png', '*.gif']:
+            pictures.extend([os.path.basename(f) for f in glob.glob(os.path.join(p, ext))])
+        return {"pictures": pictures}
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_sounds(self):
+        import glob
+        sounds_dir = os.path.join(p, 'sounds')
+        sounds = []
+        if os.path.exists(sounds_dir):
+            for ext in ['*.wav', '*.mp3']:
+                sounds.extend([os.path.basename(f) for f in glob.glob(os.path.join(sounds_dir, ext))])
+        return {"sounds": sounds}
 
 # Function to run on a separate thread every 5 hours
 def periodic_task1():
@@ -51,10 +105,11 @@ def periodic_task2():
 # Start the periodic task in a background thread
 thread = threading.Thread(target=periodic_task1, daemon=True)
 thread.start()
-thread = threading.Thread(target=periodic_task, daemon=True)
+thread = threading.Thread(target=periodic_task2, daemon=True)
 thread.start()
 
 
 # Start CherryPy server
 cherrypy.server.socket_host = '0.0.0.0'
+cherrypy.server.socket_port = 8080
 cherrypy.quickstart(Wiadom())
--- wiadomWindow.py
+++ wiadomWindow.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageTk
 import pyttsx3
 import random
 import cherrypy
+import requests
+import sys
 class GUIWindow():
     def __init__(self):
         pass
@@ -12,28 +14,35 @@ class GUIWindow():
         import glob, os,random
         os.chdir(path)
         return os.path.join(path,random.choice(glob.glob("*.gif")+glob.glob("*.jpg")+glob.glob("*.png")))
+    
+    def get_selected_pic(self, path, picture_name):
+        if picture_name == 'random':
+            return self.get_random_pic(path)
+        else:
+            return os.path.join(path, picture_name)
 
-    def open_window(self, msg, imagesPath):
-        randomPic = self.get_random_pic(imagesPath)
-        print(randomPic)
+    def send_response(self, response):
+        try:
+            requests.post('http://localhost:8080/response', json={'response': response})
+        except:
+            pass  # If server is not running, just ignore
+
+    def open_window(self, msg, imagesPath, selectedPic='random'):
+        if len(sys.argv) > 3:
+            selectedPic = sys.argv[3]
+        chosenPic = self.get_selected_pic(imagesPath, selectedPic)
+        print(f"Using picture: {chosenPic}")
         root = Tk()
 
         # This is the section of code which creates the main window
-        # w = 492
-        # h = 138
-        # ws = root.winfo_screenwidth()
-        # hs = root.winfo_screenheight()
-        # x = (ws / 2) - (w / 2)
-        # y = (hs / 2) - (h / 2)
-        # root.geometry('+%d+%d' % (x, y))
         random_color = random.choice(['#EE3B3B', '#FFC300', '#DAF7A6', '#FF5733', '#C70039', '#900C3F', '#581845', '#28A745'])
-        root.geometry('492x138')
+        root.geometry('492x180')  # Made taller for buttons
         root.configure(background=random_color)
         root.title('Wiadom.')
 
         # First, we create a canvas to put the picture on
-        pic = Image.open(randomPic)
-        pic=pic.resize((48,48), Image.ANTIALIAS)
+        pic = Image.open(chosenPic)
+        pic=pic.resize((48,48), Image.LANCZOS)
 
         worthAThousandWords = Canvas(root, height=48, width=48)
         # Then, we actually create the image file to use (it has to be a *.gif)
@@ -45,8 +54,23 @@ class GUIWindow():
         # This is the section of code which creates the a label
         Label(root, text=msg, bg='#F0F8FF', font=('Comic Sans MS', 12, 'normal')).place(x=110, y=55)
 
+        # Response buttons
+        def ok_response():
+            self.send_response('ok')
+            root.destroy()
+            
+        def not_ok_response():
+            self.send_response('not_ok')
+            root.destroy()
+
+        Button(root, text='👌', bg='#90EE90', font=('arial', 14, 'bold'), 
+               command=ok_response).place(x=110, y=100)
+        
+        Button(root, text='🚫', bg='#FFB6C1', font=('arial', 14, 'bold'), 
+               command=not_ok_response).place(x=250, y=100)
+
         # This is the section of code which creates a button
-        Button(root, text='X', bg='#F0F8FF', font=('arial', 12, 'normal'), command=lambda: sys.exit()).place(x=452, y=1)
+        Button(root, text='X', bg='#F0F8FF', font=('arial', 12, 'normal'), command=lambda: root.destroy()).place(x=452, y=1)
 
         # root.overrideredirect(True)
         root.wm_attributes("-topmost", 1)
